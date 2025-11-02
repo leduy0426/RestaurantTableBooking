fc52bf2a61900345cf194804c251db0d445957d3
Lê Duy <leduy0426@gmail.com>
2025-11-02 08:52:42 +0700

    Luu thay doi truoc pull

---
+++ src/java/models/Staff.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author fpt
+ */
+public class Staff {
+    public String staffName;
+    public String staffId;
+}
